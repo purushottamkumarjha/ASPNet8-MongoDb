5b27edc262e6d98f352f6da0edde9568bf1e6c54
Kaushik Roy Chowdhury <devedkaushik@gmail.com>
2022-10-21 12:37:36 +1300

    Added files to the working aspnet core 3.1

---
+++ MVC_Application_Complete/MongoDb.ASP.NETCore3CRUDSample.DataAccess/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace MongoDb.ASP.NETCore3CRUDSample.DataAccess.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Customer
+    { 
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement]
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than 0")]
+        public int CustomerID { get; set; }
+
+        [BsonElement]
+        [Required]
+        public string Name { get; set; }
+
+        [BsonElement]
+        [Required]
+        [Range(15, 100, ErrorMessage = "Age should be between 15 and 100 years" )]
+        public int Age { get; set; }
+
+        [BsonElement]
+        [Required]
+        [Range(1000, 10000)]
+        public int Salary { get; set; }
+    }
+}
